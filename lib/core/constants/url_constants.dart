329b5e4ddb13c9d191e9d8d46766c8c6b2807dd7
YuraZaripa <161350035+YuraZaripa@users.noreply.github.com>
2024-06-02 20:30:05 +0300

    added dismissible instead gesture detector

--- lib/core/constants/url_constants.dart
+++ lib/core/constants/url_constants.dart
@@ -3,8 +3,7 @@ import 'package:expiration_date/core/secrets.dart';
 sealed class UrlConstants {
 
   //change ip
-  static const String baseUrl = 'http://$IP_ADDRES:8080/product-expiration-date/api/v1';
-  // static const String baseUrl = 'http://localhost:8080/product-axpiration-date/api/v1';
+  static String baseUrl = 'http://${IP_ADDRES}:8080/product-expiration-date/api/v1';
 
   static const String suppliersUrl = '/suppliers';
   static const String suppliersByConditionUrl = '/suppliers/byReturnConditionType';
